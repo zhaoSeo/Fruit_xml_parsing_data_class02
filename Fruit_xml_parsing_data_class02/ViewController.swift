3967a9b636687b6ee2a1745831681c5e6ac22b3e
Seo Sang won <33479927+zhaoSeo@users.noreply.github.com>
2018-10-01 15:22:24 +0900

    Initial Commit

---
+++ Fruit_xml_parsing_data_class02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fruit_xml_parsing_data_class02
+//
+//  Created by Sang won Seo on 01/10/2018.
+//  Copyright © 2018 Sang won Seo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Fruit_xml_parsing_data_class02Tests/Fruit_xml_parsing_data_class02Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Fruit_xml_parsing_data_class02Tests.swift
+//  Fruit_xml_parsing_data_class02Tests
+//
+//  Created by Sang won Seo on 01/10/2018.
+//  Copyright © 2018 Sang won Seo. All rights reserved.
+//
+
+import XCTest
+@testable import Fruit_xml_parsing_data_class02
+
+class Fruit_xml_parsing_data_class02Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
